ee658532dbfcd0457c5a1533a22a8d713ee80daa
Vitória Vitoria <vitoriasilva98@hotmail.com>
2026-01-01 10:31:48 -0300

    feat: Criação do projeto kotlin

---
+++ src/test/kotlin/br/com/manafood/manafoodorder/ManafoodorderApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.manafood.manafoodorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ManafoodorderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
