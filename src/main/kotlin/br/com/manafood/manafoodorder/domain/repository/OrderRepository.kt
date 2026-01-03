7e88f244f755e0f274e498467aefccd5828c6480
Vitória Vitoria <vitoriasilva98@hotmail.com>
2026-01-03 13:26:56 -0300

    refactor: ajustando query nos repositories

--- src/main/kotlin/br/com/manafood/manafoodorder/domain/repository/OrderRepository.kt
+++ src/main/kotlin/br/com/manafood/manafoodorder/domain/repository/OrderRepository.kt
@@ -1,5 +1,9 @@
 package br.com.manafood.manafoodorder.domain.repository
 
+import br.com.manafood.manafoodorder.domain.common.Paged
 import br.com.manafood.manafoodorder.domain.model.Order
 
-interface OrderRepository : BaseRepository<Order>
+interface OrderRepository : BaseRepository<Order> {
+
+    fun findByOrdersReadyForKitchen(page: Int, pageSize: Int) : Paged<Order>
+}
--- src/main/kotlin/br/com/manafood/manafoodorder/infrastructure/persistence/adapter/OrderJpaRepositoryAdapter.kt
+++ src/main/kotlin/br/com/manafood/manafoodorder/infrastructure/persistence/adapter/OrderJpaRepositoryAdapter.kt
@@ -3,7 +3,9 @@ package br.com.manafood.manafoodorder.infrastructure.persistence.adapter
 import br.com.manafood.manafoodorder.domain.common.Paged
 import br.com.manafood.manafoodorder.domain.model.Order
 import br.com.manafood.manafoodorder.domain.repository.OrderRepository
+import br.com.manafood.manafoodorder.infrastructure.persistence.mapper.OrderEntityMapper
 import br.com.manafood.manafoodorder.infrastructure.persistence.repository.OrderJpaRepository
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
@@ -11,26 +13,49 @@ import java.util.UUID
 class OrderJpaRepositoryAdapter(
     private val springDataRepository: OrderJpaRepository
 ) : OrderRepository {
+
+    override fun save(entity: Order): Order {
+        val saved = springDataRepository.save(
+            OrderEntityMapper.toEntity(entity)
+        )
+        return OrderEntityMapper.toDomain(saved)
+    }
+
+    override fun deleteById(id: UUID): Boolean {
+        springDataRepository.deleteById(id)
+
+        val exists = springDataRepository.existsByIdAndNotDeleted(id)
+        return !exists
+    }
+
     override fun findById(id: UUID): Order? {
-        TODO("Not yet implemented")
+        return springDataRepository.findByIdAndNotDeleted(id)
+            .map { OrderEntityMapper.toDomain(it) }
+            .orElse(null)
+    }
+
+    override fun findByIds(ids: List<UUID>): List<Order> {
+        return springDataRepository.findAllByIdAndNotDeleted(ids)
+            .map { OrderEntityMapper.toDomain(it) }
     }
 
     override fun findPaged(
         page: Int,
         pageSize: Int
     ): Paged<Order> {
-        TODO("Not yet implemented")
+        val pageable = PageRequest.of(page, pageSize)
+        val ordersPaged = springDataRepository.findPaged(pageable)
+
+        return OrderEntityMapper.toPagedDomain(ordersPaged)
     }
 
-    override fun findByIds(ids: List<UUID>): List<Order> {
-        TODO("Not yet implemented")
-    }
+    override fun findByOrdersReadyForKitchen(
+        page: Int,
+        pageSize: Int
+    ): Paged<Order> {
+        val pageable = PageRequest.of(page, pageSize)
+        val ordersPaged = springDataRepository.findByOrdersReadyForKitchenPaged(pageable)
 
-    override fun save(entity: Order): Order {
-        TODO("Not yet implemented")
-    }
-
-    override fun deleteById(id: UUID): Boolean {
-        TODO("Not yet implemented")
+        return OrderEntityMapper.toPagedDomain(ordersPaged)
     }
 }
